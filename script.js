94310d757920ccba0cc110fdf66fef5f4c223320
mckinley9 <mckinleylaberge@gmail.com>
2024-08-09 12:46:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function checkPassword() {
-    const correctPassword = "phished";
+    const correctPassword = "GonePhishing";
     const enteredPassword = document.getElementById("password").value;
     const message = document.getElementById("message");
 
     if (enteredPassword === correctPassword) {
-        window.open("https://sites.google.com/uchicago.edu/site/social-engineering", "_blank");
+        window.open("https://sites.google.com/uchicago.edu/site/phishing-recap", "_blank");
         message.textContent = "";
     } else {
         message.textContent = "Incorrect password. Please try again.";
